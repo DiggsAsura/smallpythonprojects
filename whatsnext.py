8abc51f4c6ba5bcc249fe7b83b62661126849d12
Kenneth B. Bjerke <kenneth.bjerke@gmail.com>
2022-02-10 23:56:59 +0100

    Add files via upload

---
+++ whatsnext.py
@@ -0,0 +1,9 @@
+# Okay been doing this for a month. Feel some progression, and also started thinking about
+# my first real project. A web scraping thing.
+# It's gonna be a long while I suspect, but at least some ideas.
+# Today it did feel too rough to start codecademy though, next up is offplatform project
+# which last time was a roayl pain in the ass. 
+# 
+# I haaaaate jypter notebook so hard too.
+#
+# Will come back stronger though!
