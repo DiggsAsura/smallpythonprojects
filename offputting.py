2b807b0c3072982e38e3f1f7217b6f268492fdb5
Kenneth B. Bjerke <kenneth.bjerke@gmail.com>
2022-02-12 22:47:35 +0100

    Add files via upload

---
+++ offputting.py
@@ -0,0 +1,11 @@
+# The off-platform projects, after the linar regression one early on 
+# in Codecademy just totally killed my motivation for doing anything more
+# on Codecademy. 
+
+# Now it's another one up, and I have been off 3 days in a row. And just
+# can't seem to be motivated to start it. 
+# 
+# Those projects is a bit mis step for Codecademy imo. Jupyter notebook
+# is not cool. It fucking sucks. 
+
+
